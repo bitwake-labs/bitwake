11990ed9c473f792394d06553a57e420a3af9f2e
devduds96 <contato.eduardo96@gmail.com>
2024-10-28 23:20:54 -0300

    Withdraw screen with containers and CSS setup - Withdraw Screen version 3

--- app/withdraw.tsx
+++ app/withdraw.tsx
@@ -27,22 +27,51 @@ export default function Withdraw() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.saldoText}>Saldo disponível</Text>
+<View style={styles.container}>
+  <View style={styles.containerHead}>
+    <View style={styles.rowContainer}>
+      <View style={styles.rowleft}>
+        <Text style={styles.labeltopleftbit}>Bit</Text>
+        <Text style={styles.labeltopleftwake}>wake</Text>
+       </View>
+          
+      <View style={styles.rowright}>
+         <Text style={styles.labeltopola}>Olá,</Text>
+         <Text style={styles.labeltopusuario}>Usuário :) </Text>
+      </View>
+   </View>
+    {/* Container de todo o corpo da página */}
+  </View>
+    <View style={styles.inputContainer}>
+      {/* Label e campo de Saldo*/}
+      <Text style={styles.saldoText}>Saldo Atual:</Text>
       <Text style={styles.saldoValue}>R$ {saldo.toFixed(2)}</Text>
       
+
+
       {/* Campo de entrada para o valor do saque */}
+      <Text style={styles.label}>Quanto você deseja sacar hoje?</Text>
       <TextInput
         style={styles.input}
-        placeholder="Digite o valor do saque"
+        placeholder="Valor para sacar"
         keyboardType="numeric"
         value={saque}
         onChangeText={(text) => setSaque(text)}
       />
 
+        {/* Campo para colocar a chave Criptografada para autorizar o saque */}
+        <Text style={styles.label}>Insira a chave criptografada:</Text>
+        <TextInput
+        style={styles.input}
+        placeholder="Insira a chave aqui"
+        keyboardType="numeric"
+        //value={saque}
+        //onChangeText={(text) => setSaque(text)}
+      />
+    </View>
       {/* Botão de saque */}
       <TouchableOpacity style={styles.saqueButton} onPress={handleSaque}>
-        <Text style={styles.saqueButtonText}>Sacar</Text>
+        <Text style={styles.saqueButtonText}>Sacar ;)</Text>
       </TouchableOpacity>
       
       {/* Lista de histórico de saques */}
@@ -56,43 +85,120 @@ export default function Withdraw() {
         )}
         style={styles.historyContainer}
       />
-    </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
-    padding: 20,
+    alignItems: 'center',
+    padding: 10,
     backgroundColor: '#f8f9fa',
     flex: 1,
   },
-  saldoText: {
-    fontSize: 18,
+
+  containerHead: {
+    padding: 0,
+    width: '100%',
+    //alignItems: 'center',
+    //backgroundColor: '#f8f9fa',
+    flex: 1,
+  },
+
+  inputContainer: {
+    width: '90%',
+    backgroundColor: '#ebeaea', // Cor do fundo cinza
+    padding: 15,
+    borderRadius: 10, // Bordas arredondadas
+    marginTop: -70, // Ajuste negativo para subir o container
+  },
+  rowContainer: {
+    flexDirection: 'row', // Coloca os dois conjuntos de texto na mesma linha
+    justifyContent: 'space-between', // Alinha um grupo à esquerda e outro à direita
+    alignItems: 'center', // Alinha verticalmente ao centro
+    paddingHorizontal: 10,
+    //marginBottom: 10,
+  },
+
+  rowleft: {
+    flexDirection: 'row', // Coloca os itens em linha
+    alignItems: 'center',
+    //marginBottom: 10,
+    justifyContent: 'flex-start', // Alinha a linha à esquerda
+  },
+
+  rowright: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end', // Alinha a linha à direita
+  },
+  labeltopleftbit: {
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 0,
+    color: '#ff5f49',
+  },
+
+  labeltopleftwake: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 0,
+    color: '#8c6d5b',
+  },
+
+  labeltopola:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 0,
+    color: '#ff5f49',
+  },
+
+  labeltopusuario:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 0,
+    color: '#8c6d5b',
+  },
+
+  saldoText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: -8,
+    color: '#8c6d5b',
   },
   saldoValue: {
     fontSize: 32,
     fontWeight: 'bold',
-    color: 'green',
-    marginBottom: 20,
+    color: '#ff5f49',
+    marginBottom: 15,
   },
+
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#8c6d5b',
+  },
+
   input: {
     height: 50,
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 15,
-    fontSize: 18,
+    fontSize: 16,
     marginBottom: 20,
+    backgroundColor: 'white',
   },
   saqueButton: {
-    backgroundColor: 'orange',
+    backgroundColor: '#ff5f49',
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 10,
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: -5,
+    marginTop: 10, //Margem superior do botão
   },
   saqueButtonText: {
     color: '#fff',
@@ -105,7 +211,7 @@ const styles = StyleSheet.create({
   card: {
     padding: 15,
     borderRadius: 10,
-    marginBottom: 10,
+    marginBottom: 6,
   },
   cardText: {
     color: 'white',
